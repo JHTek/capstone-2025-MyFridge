784ee05468eb8ca2a9d120e4043ec9d98ed6eb51
nugungam <baghyunbin01@naver.com>
2025-04-30 18:17:15 +0900

    라이브러리 오류 수정(강제 SNAKE_CASE)

--- backend/sts/Ref/src/main/java/com/mysite/ref/dto/RefrigeratorDTO.java
+++ backend/sts/Ref/src/main/java/com/mysite/ref/dto/RefrigeratorDTO.java
@@ -1,10 +1,14 @@
 package com.mysite.ref.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class RefrigeratorDTO {
 	
 	private String refrigeratorName;
